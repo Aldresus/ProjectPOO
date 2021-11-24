a736d8d3b3a4bc03ca8458ab6479b44f1023be79
Hugo Champy <Hugo Champy@DESKTOP-22GT46J>
2021-11-24 14:05:34 +0100

    Ajoutez des fichiers projet.

---
+++ ProjectPOO/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+
+//MyForm.cpp 
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int __clrcall WinMain(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ProjectPOO::MyForm monFormulaire;//ATTENTION : ici testWin est le nom du projet. Vous devez l’adapter au nom de votre projet 
+	Application::Run(%monFormulaire);
+}
