ca40f63848ceaf50fd5f7512fb3b664012ecfc9e
Maxime EY <94915705+Likasaa@users.noreply.github.com>
2021-11-29 09:30:56 +0100

    Update MyForm.h

--- ProjectPOO/MyForm.h
+++ ProjectPOO/MyForm.h
@@ -1,6 +1,6 @@
-#pragma once
+#include "CLservices.h"
 
-namespace ProjectPOO {
+namespace Prosit_6 {
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -10,7 +10,7 @@ namespace ProjectPOO {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Description résumée de MyForm
+	/// Description rÃ©sumÃ©e de MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -25,7 +25,7 @@ namespace ProjectPOO {
 
 	protected:
 		/// <summary>
-		/// Nettoyage des ressources utilisées.
+		/// Nettoyage des ressources utilisÃ©es.
 		/// </summary>
 		~MyForm()
 		{
@@ -34,26 +34,199 @@ namespace ProjectPOO {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::DataGridView^ dgv_enr;
+	protected:
+
+	private: System::Windows::Forms::Button^ btn_load;
+	private: System::Windows::Forms::Button^ btn_insert;
+	private: System::Windows::Forms::Button^ btn_delete;
+	private: System::Windows::Forms::Button^ btn_update;
+	private: System::Windows::Forms::TextBox^ txt_id;
+	private: System::Windows::Forms::TextBox^ txt_nom_employe;
+	private: System::Windows::Forms::TextBox^ txt_prenom_employe;
+	private: System::Windows::Forms::TextBox^ txt_date_de_naissance_employe;
+
+
+
+	private: NS_Comp_Svc::CLservices^ oSvc;
+	private: System::Data::DataSet^ oDs;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
-		/// Variable nécessaire au concepteur.
+		/// Variable nÃ©cessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
-		/// le contenu de cette méthode avec l'éditeur de code.
+		/// MÃ©thode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette mÃ©thode avec l'Ã©diteur de code.
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->dgv_enr = (gcnew System::Windows::Forms::DataGridView());
+			this->btn_load = (gcnew System::Windows::Forms::Button());
+			this->btn_insert = (gcnew System::Windows::Forms::Button());
+			this->btn_delete = (gcnew System::Windows::Forms::Button());
+			this->btn_update = (gcnew System::Windows::Forms::Button());
+			this->txt_id = (gcnew System::Windows::Forms::TextBox());
+			this->txt_nom_employe = (gcnew System::Windows::Forms::TextBox());
+			this->txt_prenom_employe = (gcnew System::Windows::Forms::TextBox());
+			this->txt_date_de_naissance_employe = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgv_enr))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dgv_enr
+			// 
+			this->dgv_enr->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dgv_enr->Location = System::Drawing::Point(16, 15);
+			this->dgv_enr->Margin = System::Windows::Forms::Padding(4);
+			this->dgv_enr->Name = L"dgv_enr";
+			this->dgv_enr->RowHeadersWidth = 51;
+			this->dgv_enr->Size = System::Drawing::Size(629, 185);
+			this->dgv_enr->TabIndex = 0;
+			// 
+			// btn_load
+			// 
+			this->btn_load->Location = System::Drawing::Point(20, 225);
+			this->btn_load->Margin = System::Windows::Forms::Padding(4);
+			this->btn_load->Name = L"btn_load";
+			this->btn_load->Size = System::Drawing::Size(100, 159);
+			this->btn_load->TabIndex = 1;
+			this->btn_load->Text = L"Load DB";
+			this->btn_load->UseVisualStyleBackColor = true;
+			this->btn_load->Click += gcnew System::EventHandler(this, &MyForm::btn_load_Click);
+			// 
+			// btn_insert
+			// 
+			this->btn_insert->Location = System::Drawing::Point(128, 225);
+			this->btn_insert->Margin = System::Windows::Forms::Padding(4);
+			this->btn_insert->Name = L"btn_insert";
+			this->btn_insert->Size = System::Drawing::Size(100, 46);
+			this->btn_insert->TabIndex = 2;
+			this->btn_insert->Text = L"INS";
+			this->btn_insert->UseVisualStyleBackColor = true;
+			this->btn_insert->Click += gcnew System::EventHandler(this, &MyForm::btn_insert_Click);
+			// 
+			// btn_delete
+			// 
+			this->btn_delete->Location = System::Drawing::Point(128, 338);
+			this->btn_delete->Margin = System::Windows::Forms::Padding(4);
+			this->btn_delete->Name = L"btn_delete";
+			this->btn_delete->Size = System::Drawing::Size(100, 46);
+			this->btn_delete->TabIndex = 3;
+			this->btn_delete->Text = L"DEL";
+			this->btn_delete->UseVisualStyleBackColor = true;
+			this->btn_delete->Click += gcnew System::EventHandler(this, &MyForm::btn_delete_Click);
+			// 
+			// btn_update
+			// 
+			this->btn_update->Location = System::Drawing::Point(128, 282);
+			this->btn_update->Margin = System::Windows::Forms::Padding(4);
+			this->btn_update->Name = L"btn_update";
+			this->btn_update->Size = System::Drawing::Size(100, 46);
+			this->btn_update->TabIndex = 4;
+			this->btn_update->Text = L"UPD";
+			this->btn_update->UseVisualStyleBackColor = true;
+			this->btn_update->Click += gcnew System::EventHandler(this, &MyForm::btn_update_Click);
+			// 
+			// txt_id
+			// 
+			this->txt_id->Location = System::Drawing::Point(236, 225);
+			this->txt_id->Margin = System::Windows::Forms::Padding(4);
+			this->txt_id->Name = L"txt_id";
+			this->txt_id->Size = System::Drawing::Size(408, 22);
+			this->txt_id->TabIndex = 5;
+			// 
+			// txt_nom_employe
+			// 
+			this->txt_nom_employe->Location = System::Drawing::Point(236, 257);
+			this->txt_nom_employe->Margin = System::Windows::Forms::Padding(4);
+			this->txt_nom_employe->Name = L"txt_nom_employe";
+			this->txt_nom_employe->Size = System::Drawing::Size(408, 22);
+			this->txt_nom_employe->TabIndex = 6;
+			// 
+			// txt_prenom_employe
+			// 
+			this->txt_prenom_employe->Location = System::Drawing::Point(236, 293);
+			this->txt_prenom_employe->Margin = System::Windows::Forms::Padding(4);
+			this->txt_prenom_employe->Name = L"txt_prenom_employe";
+			this->txt_prenom_employe->Size = System::Drawing::Size(408, 22);
+			this->txt_prenom_employe->TabIndex = 7;
+			// 
+			// txt_date_de_naissance_employe
+			// 
+			this->txt_date_de_naissance_employe->Location = System::Drawing::Point(266, 323);
+			this->txt_date_de_naissance_employe->Margin = System::Windows::Forms::Padding(4);
+			this->txt_date_de_naissance_employe->Name = L"txt_date_de_naissance_employe";
+			this->txt_date_de_naissance_employe->Size = System::Drawing::Size(408, 22);
+			this->txt_date_de_naissance_employe->TabIndex = 8;
+			// 
+			// btn load table adresse
+			// 
+			this->button1->Location = System::Drawing::Point(309, 338);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(116, 45);
+			this->button1->TabIndex = 9;
+			this->button1->Text = L"Load table adresse";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(675, 409);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->txt_date_de_naissance_employe);
+			this->Controls->Add(this->txt_prenom_employe);
+			this->Controls->Add(this->txt_nom_employe);
+			this->Controls->Add(this->txt_id);
+			this->Controls->Add(this->btn_update);
+			this->Controls->Add(this->btn_delete);
+			this->Controls->Add(this->btn_insert);
+			this->Controls->Add(this->btn_load);
+			this->Controls->Add(this->dgv_enr);
+			this->Margin = System::Windows::Forms::Padding(4);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgv_enr))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void btn_load_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->dgv_enr->Refresh();
+		this->oDs = this->oSvc->selectionnerTousLesEmployes("Rsl");
+		this->dgv_enr->DataSource = this->oDs;
+		this->dgv_enr->DataMember = "Rsl";
+	}
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->oSvc = gcnew NS_Comp_Svc::CLservices();
+	}
+	private: System::Void btn_insert_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->oSvc->ajouterUnEmploye(Convert::ToString(this->txt_nom_employe->Text), Convert::ToString(this->txt_prenom_employe->Text), Convert::ToDateTime(this->txt_date_de_naissance_employe));
+	}
+	private: System::Void btn_update_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->oSvc->updateUnEmploye(Convert::ToInt32(this->txt_id->Text), Convert::ToString(this->txt_nom_employe->Text), Convert::ToString(this->txt_prenom_employe->Text), Convert::ToDateTime(this->txt_date_de_naissance_employe));
+	}
+	private: System::Void btn_delete_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->oSvc->supprimerEmployes();
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->dgv_enr->Refresh();
+		this->oDs = this->oSvc->selectionnerTousLesEmployes1("Rsl");
+		this->dgv_enr->DataSource = this->oDs;
+		this->dgv_enr->DataMember = "Rsl";
+	}
 	};
 }
+
