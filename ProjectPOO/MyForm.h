f89ed35c556807b4aacaa130763b3684e290b0be
Hugo CHAMPY <hugo.champy@viacesi.fr>
2021-11-29 15:46:14 +0100

    Création de l'interface

--- ProjectPOO/MyForm.h
+++ ProjectPOO/MyForm.h
@@ -34,16 +34,32 @@ namespace ProjectPOO {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
+
+
+
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::TextBox^ textBox8;
+
+
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button4;
-	private: System::Windows::Forms::DataGridView^ dataGridView1;
-	private: System::Windows::Forms::TextBox^ textBox1;
 
-	private: System::Windows::Forms::TextBox^ textBox3;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
+
+
+
+
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	protected:
 
 	private:
@@ -59,122 +75,212 @@ namespace ProjectPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(12, 12);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(527, 194);
+			this->dataGridView1->TabIndex = 0;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
+			// 
+			// textBox2
+			// 
+			this->textBox2->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox2->Location = System::Drawing::Point(595, 186);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(370, 20);
+			this->textBox2->TabIndex = 5;
+			this->textBox2->Text = L"Seuil réapro";
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this->textBox3->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox3->Location = System::Drawing::Point(595, 161);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(370, 20);
+			this->textBox3->TabIndex = 6;
+			this->textBox3->Text = L"Quantité stocks";
+			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox3_TextChanged);
+			// 
+			// textBox4
+			// 
+			this->textBox4->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox4->Location = System::Drawing::Point(595, 31);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(370, 20);
+			this->textBox4->TabIndex = 10;
+			this->textBox4->Text = L"Reference";
+			this->textBox4->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox4_TextChanged);
+			// 
+			// textBox5
+			// 
+			this->textBox5->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox5->Location = System::Drawing::Point(595, 57);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(370, 20);
+			this->textBox5->TabIndex = 9;
+			this->textBox5->Text = L"Nom article";
+			this->textBox5->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox5_TextChanged);
+			// 
+			// textBox6
+			// 
+			this->textBox6->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox6->Location = System::Drawing::Point(595, 83);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(370, 20);
+			this->textBox6->TabIndex = 8;
+			this->textBox6->Text = L"Prix produit HT";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(592, 12);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(52, 16);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Article";
+			// 
+			// textBox7
+			// 
+			this->textBox7->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox7->Location = System::Drawing::Point(595, 109);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(370, 20);
+			this->textBox7->TabIndex = 14;
+			this->textBox7->Text = L"Couleur";
+			this->textBox7->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox7_TextChanged);
+			// 
+			// textBox8
+			// 
+			this->textBox8->ForeColor = System::Drawing::Color::DarkGray;
+			this->textBox8->Location = System::Drawing::Point(595, 135);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(370, 20);
+			this->textBox8->TabIndex = 13;
+			this->textBox8->Text = L"Nature";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(3, 134);
+			this->button1->Location = System::Drawing::Point(12, 230);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(49, 115);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Size = System::Drawing::Size(120, 149);
+			this->button1->TabIndex = 15;
+			this->button1->Text = L"Load";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(58, 175);
+			this->button2->Location = System::Drawing::Point(153, 230);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(72, 34);
-			this->button2->TabIndex = 1;
-			this->button2->Text = L"button2";
+			this->button2->Size = System::Drawing::Size(99, 40);
+			this->button2->TabIndex = 16;
+			this->button2->Text = L"Insert";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(58, 134);
+			this->button3->Location = System::Drawing::Point(153, 284);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(72, 34);
-			this->button3->TabIndex = 2;
-			this->button3->Text = L"button3";
+			this->button3->Size = System::Drawing::Size(99, 40);
+			this->button3->TabIndex = 17;
+			this->button3->Text = L"Update";
 			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(58, 215);
+			this->button4->Location = System::Drawing::Point(153, 339);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(72, 34);
-			this->button4->TabIndex = 3;
-			this->button4->Text = L"button4";
+			this->button4->Size = System::Drawing::Size(99, 40);
+			this->button4->TabIndex = 18;
+			this->button4->Text = L"Delete";
 			this->button4->UseVisualStyleBackColor = true;
 			// 
-			// dataGridView1
+			// pictureBox1
 			// 
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(3, 12);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(268, 112);
-			this->dataGridView1->TabIndex = 4;
-			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
-			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(148, 142);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(110, 20);
-			this->textBox1->TabIndex = 5;
-			// 
-			// textBox3
-			// 
-			this->textBox3->Location = System::Drawing::Point(148, 194);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(110, 20);
-			this->textBox3->TabIndex = 7;
-			// 
-			// textBox4
-			// 
-			this->textBox4->Location = System::Drawing::Point(148, 168);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(110, 20);
-			this->textBox4->TabIndex = 8;
-			// 
-			// textBox2
-			// 
-			this->textBox2->Location = System::Drawing::Point(148, 223);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(110, 20);
-			this->textBox2->TabIndex = 6;
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBox1->ImageLocation = L"https://media.discordapp.net/attachments/881518424663158826/914885743392989214/me"
+				L"at.png";
+			this->pictureBox1->Location = System::Drawing::Point(652, 251);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(313, 110);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 22;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->textBox3);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->dataGridView1);
+			this->ClientSize = System::Drawing::Size(992, 385);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->dataGridView1);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"GestionStocks.exe";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	private: System::Void textBox4_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox13_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox5_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox7_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
-
