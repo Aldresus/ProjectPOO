02b37768662991328073cbe29360bbb846b2367c
Hugo Champy <63045484+Aldresus@users.noreply.github.com>
2021-12-01 21:22:05 +0100

    Encore un nouveau MCD

--- codeSQL.sql
+++ codeSQL.sql
@@ -46,33 +46,6 @@ CREATE TABLE Client(
 
 
 /*------------------------------------------------------------
--- Table: Employe
-------------------------------------------------------------*/
-CREATE TABLE Employe(
-	ID_employe               INT IDENTITY (1,1) NOT NULL ,
-	nom_employe              VARCHAR (50) NOT NULL ,
-	prenom_employe           VARCHAR (50) NOT NULL ,
-	date_naissance_employe   DATETIME NOT NULL ,
-	adresse_employe          VARCHAR (50) NOT NULL ,
-	id_superviseur           INT  NOT NULL  ,
-	CONSTRAINT Employe_PK PRIMARY KEY (ID_employe)
-);
-
-
-/*------------------------------------------------------------
--- Table: Stocks
-------------------------------------------------------------*/
-CREATE TABLE Stocks(
-	id_stocks           INT IDENTITY (1,1) NOT NULL ,
-	quantite_stock      INT  NOT NULL ,
-	seuil_reapro        INT  NOT NULL ,
-	demarque_inconnue   FLOAT  NOT NULL ,
-	reference_article   INT  NOT NULL  ,
-	CONSTRAINT Stocks_PK PRIMARY KEY (id_stocks)
-);
-
-
-/*------------------------------------------------------------
 -- Table: Commande
 ------------------------------------------------------------*/
 CREATE TABLE Commande(
@@ -82,9 +55,10 @@ CREATE TABLE Commande(
 	date_commande        DATETIME NOT NULL ,
 	date_livraison       DATETIME NOT NULL ,
 	adresse_livraison    VARCHAR (50) NOT NULL ,
-	id_client            INT  NOT NULL ,
-	ID_facture           INT  NOT NULL  ,
+	id_client            INT  NOT NULL  ,
 	CONSTRAINT Commande_PK PRIMARY KEY (reference_commande)
+
+	,CONSTRAINT Commande_Client_FK FOREIGN KEY (id_client) REFERENCES Client(id_client)
 );
 
 
@@ -100,6 +74,24 @@ CREATE TABLE Facture(
 	adresse_facturation   VARCHAR (50) NOT NULL ,
 	reference_commande    VARCHAR (255) NOT NULL  ,
 	CONSTRAINT Facture_PK PRIMARY KEY (ID_facture)
+
+	,CONSTRAINT Facture_Commande_FK FOREIGN KEY (reference_commande) REFERENCES Commande(reference_commande)
+);
+
+
+/*------------------------------------------------------------
+-- Table: Employe
+------------------------------------------------------------*/
+CREATE TABLE Employe(
+	ID_employe               INT IDENTITY (1,1) NOT NULL ,
+	nom_employe              VARCHAR (50) NOT NULL ,
+	prenom_employe           VARCHAR (50) NOT NULL ,
+	date_naissance_employe   DATETIME NOT NULL ,
+	adresse_employe          VARCHAR (50) NOT NULL ,
+	id_superviseur           INT  NOT NULL  ,
+	CONSTRAINT Employe_PK PRIMARY KEY (ID_employe)
+
+	,CONSTRAINT Employe_Superviseurs_FK FOREIGN KEY (id_superviseur) REFERENCES Superviseurs(id_superviseur)
 );
 
 
@@ -113,6 +105,24 @@ CREATE TABLE Paiement(
 	moyen_paiement    VARCHAR (50) NOT NULL ,
 	ID_facture        INT  NOT NULL  ,
 	CONSTRAINT Paiement_PK PRIMARY KEY (id_payement)
+
+	,CONSTRAINT Paiement_Facture_FK FOREIGN KEY (ID_facture) REFERENCES Facture(ID_facture)
+);
+
+
+/*------------------------------------------------------------
+-- Table: Stocks
+------------------------------------------------------------*/
+CREATE TABLE Stocks(
+	id_stocks           INT IDENTITY (1,1) NOT NULL ,
+	quantite_stock      INT  NOT NULL ,
+	seuil_reapro        INT  NOT NULL ,
+	demarque_inconnue   FLOAT  NOT NULL ,
+	reference_article   INT  NOT NULL  ,
+	CONSTRAINT Stocks_PK PRIMARY KEY (id_stocks)
+
+	,CONSTRAINT Stocks_Article_FK FOREIGN KEY (reference_article) REFERENCES Article(reference_article)
+	,CONSTRAINT Stocks_Article_AK UNIQUE (reference_article)
 );
 
 
@@ -124,59 +134,10 @@ CREATE TABLE Composer(
 	reference_commande   VARCHAR (255) NOT NULL ,
 	quantite             INT  NOT NULL  ,
 	CONSTRAINT Composer_PK PRIMARY KEY (reference_article,reference_commande)
+
+	,CONSTRAINT Composer_Article_FK FOREIGN KEY (reference_article) REFERENCES Article(reference_article)
+	,CONSTRAINT Composer_Commande0_FK FOREIGN KEY (reference_commande) REFERENCES Commande(reference_commande)
 );
 
 
 
-
-ALTER TABLE Employe
-	ADD CONSTRAINT Employe_Superviseurs0_FK
-	FOREIGN KEY (id_superviseur)
-	REFERENCES Superviseurs(id_superviseur);
-
-ALTER TABLE Stocks
-	ADD CONSTRAINT Stocks_Article0_FK
-	FOREIGN KEY (reference_article)
-	REFERENCES Article(reference_article);
-
-ALTER TABLE Stocks 
-	ADD CONSTRAINT Stocks_Article0_AK 
-	UNIQUE (reference_article);
-
-ALTER TABLE Commande
-	ADD CONSTRAINT Commande_Client0_FK
-	FOREIGN KEY (id_client)
-	REFERENCES Client(id_client);
-
-ALTER TABLE Commande
-	ADD CONSTRAINT Commande_Facture1_FK
-	FOREIGN KEY (ID_facture)
-	REFERENCES Facture(ID_facture);
-
-ALTER TABLE Commande 
-	ADD CONSTRAINT Commande_Facture0_AK 
-	UNIQUE (ID_facture);
-
-ALTER TABLE Facture
-	ADD CONSTRAINT Facture_Commande0_FK
-	FOREIGN KEY (reference_commande)
-	REFERENCES Commande(reference_commande);
-
-ALTER TABLE Facture 
-	ADD CONSTRAINT Facture_Commande0_AK 
-	UNIQUE (reference_commande);
-
-ALTER TABLE Paiement
-	ADD CONSTRAINT Paiement_Facture0_FK
-	FOREIGN KEY (ID_facture)
-	REFERENCES Facture(ID_facture);
-
-ALTER TABLE Composer
-	ADD CONSTRAINT Composer_Article0_FK
-	FOREIGN KEY (reference_article)
-	REFERENCES Article(reference_article);
-
-ALTER TABLE Composer
-	ADD CONSTRAINT Composer_Commande1_FK
-	FOREIGN KEY (reference_commande)
-	REFERENCES Commande(reference_commande);
